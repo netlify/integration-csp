837f7ede3fdad0cb80aa373bf57d5d9e79a7061e
Ed Stephinson <edward.stephinson@gmail.com>
2023-08-23 19:14:42 +0100

    try prefixing plugins

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -26,7 +26,7 @@ export const onPreBuild = async ({ inputs, netlifyConfig, utils }) => {
   fs.writeFileSync(`${edgeFunctionsDir}/__csp-nonce-inputs.json`, config);
   console.log(`  Writing nonce edge function to ${edgeFunctionsDir}...`);
   const nonceSource =
-    "node_modules/@netlify/plugin-csp-nonce/src/__csp-nonce.ts";
+    "plugins/node_modules/@netlify/plugin-csp-nonce/src/__csp-nonce.ts";
   const nonceDest = `${edgeFunctionsDir}/__csp-nonce.ts`;
   fs.copyFileSync(nonceSource, nonceDest);
 
@@ -37,7 +37,7 @@ export const onPreBuild = async ({ inputs, netlifyConfig, utils }) => {
     await utils.run.command(`mkdir -p ${functionsDir}`);
     console.log(`  Writing violations logging function to ${functionsDir}...`);
     const violationsSource =
-      "node_modules/@netlify/plugin-csp-nonce/src/__csp-violations.ts";
+      "plugins/node_modules/@netlify/plugin-csp-nonce/src/__csp-violations.ts";
     const violationsDest = `${functionsDir}/__csp-violations.ts`;
     fs.copyFileSync(violationsSource, violationsDest);
   } else {
